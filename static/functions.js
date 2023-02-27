64426dc4c4300f246ce03428e80ca2d72477f22a
MatthewMH <matthenry@apps.ipb.ac.id>
2023-02-27 22:38:17 +0700

    loading animation complete

--- static/functions.js
+++ static/functions.js
@@ -8,6 +8,9 @@ function check_submission_conditions(event){
         document.getElementById("attachment-text").innerHTML = "No video file attached. Please insert a MP4 video!";
     }
     else {
-        document.getElementById("drop-area").innerHTML = '<div class="loader"></div>';
+        document.getElementById("attachment-text").innerHTML = "Processing. It may take a while..."
+        document.getElementById("upload-video-box").style.display = "none";
+        document.getElementById("drag-drop-message").style.display = "none";
+        document.getElementById("loading-animation").style.display = "block";
     }
 }
